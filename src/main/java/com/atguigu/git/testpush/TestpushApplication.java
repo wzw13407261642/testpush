3575dfd139006392ad267a718c2d3994588d519a
wzw <wzw@126.com>
2020-06-02 18:01:21 +0800

    idea往远程创建项目

---
+++ src/main/java/com/atguigu/git/testpush/TestpushApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.git.testpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpushApplication.class, args);
+    }
+
+}
